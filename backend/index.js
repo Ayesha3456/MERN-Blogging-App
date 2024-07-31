42de0030de565d7d8103781ad2200b03bbcdb5bb
Ayesha M <amruthanisha@gmail.com>
2024-07-31 12:27:57 +0530

    CHANGES

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
-const express = require('express'); // Import the express framework to create a web server
-const app = express(); // Create an instance of an Express application
-require('dotenv').config(); // Load environment variables from a .env file into process.env
-require('./Models/db'); // Import and execute the database configuration and connection file
-const PORT = process.env.PORT || 8080; // Set the port for the server, using the environment variable PORT or default to 8080
-const BlogRouter = require('./Routes/BlogRouter'); // Import the router for handling Blog-related routes
-const bodyParser = require('body-parser'); // Import body-parser middleware to parse incoming request bodies
-const cors = require('cors'); // Import cors middleware to enable Cross-Origin Resource Sharing
+const express = require('express');
+const app = express();
+require('dotenv').config();
+require('./Models/db');
+const PORT = process.env.PORT || 8080;
+const BlogRouter = require('./Routes/BlogRouter');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.get('/', (req, res) => { // Define a route handler for the root URL
-    res.send('Hello from the server'); // Send a simple response when accessing the root URL
+app.get('/', (req, res) => {
+    res.send('Hello from the server');
 });
-app.use(cors()); // Use the cors middleware to enable CORS
-app.use(bodyParser.json()); // Use the body-parser middleware to parse JSON request bodies
-app.use('/blog', BlogRouter); // Use the BlogRouter for handling routes starting with /blog
-
-app.listen(PORT, () => { // Start the server and listen on the specified port
-    console.log(`Server is running on PORT=${PORT}`); // Log a message indicating the server is running and the port it is listening on
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/blog', BlogRouter);
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT=${PORT}`);
 });
